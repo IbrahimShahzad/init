b8cb1f0fa4f72ba3151698a0be03a6a34ca6a2ad
Ibrahim Shahzad <48486042+IbrahimShahzad@users.noreply.github.com>
2021-07-07 18:22:12 +0500

    Update setupvim.sh

--- bash_scripts/setupvim.sh
+++ bash_scripts/setupvim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/IbrahimShahzad/My-Vimrc-file/master/.vimrc ~/.vimrc
+wget https://raw.githubusercontent.com/IbrahimShahzad/init/master/vim/.vimrc ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo -ne '\n' | vim +PluginInstall +qall
 pushd ~/.vim/bundle/YouCompleteMe/
