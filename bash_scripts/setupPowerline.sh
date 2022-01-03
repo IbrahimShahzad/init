4ffb6e10e23fc41922bc073849fd83a473c8db5f
Ibrahim Shahzad <48486042+IbrahimShahzad@users.noreply.github.com>
2022-01-03 11:37:58 +0500

    Update setupPowerline.sh

--- bash_scripts/setupPowerline.sh
+++ bash_scripts/setupPowerline.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+# add to bashrc
 echo "if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
   powerline-daemon -q
   POWERLINE_BASH_CONTINUATION=1
   POWERLINE_BASH_SELECT=1
   source /usr/share/powerline/bindings/bash/powerline.sh
 fi" >> ~/.bashrc
+
+source ~/.bashrc
+
+# Add to tmux.conf
+echo "run-shell 'powerline-config tmux setup'" >> ~/.tmux.conf
