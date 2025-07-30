5ec128a1248d9626cb14abd6e833de81dc106c9f
Ibrahim Shahzad <ibrahim.shahzad@ibrahim-de064-mac.local>
2025-07-30 20:15:58 +0200

    just dot files

--- bash_scripts/setup_pktgen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pushd /opt/
-git clone http://dpdk.org/git/apps/pktgen-dpdk
-pushd pktgen-dpdk
-dos2unix ./tools/pktgen-build.sh*
-make
-popd
-
--- bash_scripts/setupvim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/IbrahimShahzad/init/master/vim/.vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo -ne '\n' | vim +PluginInstall +qall
-pushd ~/.vim/bundle/YouCompleteMe/
-git submodule update --init --recursive
-./install.py --clang-completer
-popd
